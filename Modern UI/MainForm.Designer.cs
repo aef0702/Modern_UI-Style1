e23711c1ecd42e8b4d22c623c1e44a08ac00863f
애프 <trf203742@gmail.com>
2025-08-15 03:18:15 +0900

    Add project files.

---
+++ Modern UI/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Modern_UI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.side_panel = new System.Windows.Forms.Panel();
+            this.profile_panel = new System.Windows.Forms.Panel();
+            this.Form1_but = new System.Windows.Forms.Button();
+            this.Form2_but = new System.Windows.Forms.Button();
+            this.Form3_but = new System.Windows.Forms.Button();
+            this.Setting_but = new System.Windows.Forms.Button();
+            this.FormpaL = new System.Windows.Forms.Panel();
+            this.side_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // side_panel
+            // 
+            this.side_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(55)))), ((int)(((byte)(77)))));
+            this.side_panel.Controls.Add(this.Setting_but);
+            this.side_panel.Controls.Add(this.Form3_but);
+            this.side_panel.Controls.Add(this.Form2_but);
+            this.side_panel.Controls.Add(this.Form1_but);
+            this.side_panel.Controls.Add(this.profile_panel);
+            this.side_panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.side_panel.Location = new System.Drawing.Point(0, 0);
+            this.side_panel.Name = "side_panel";
+            this.side_panel.Size = new System.Drawing.Size(200, 490);
+            this.side_panel.TabIndex = 0;
+            // 
+            // profile_panel
+            // 
+            this.profile_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.profile_panel.Location = new System.Drawing.Point(0, 0);
+            this.profile_panel.Name = "profile_panel";
+            this.profile_panel.Size = new System.Drawing.Size(200, 92);
+            this.profile_panel.TabIndex = 0;
+            // 
+            // Form1_but
+            // 
+            this.Form1_but.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Form1_but.FlatAppearance.BorderSize = 0;
+            this.Form1_but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Form1_but.Font = new System.Drawing.Font("Paperlogy 4 Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Form1_but.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(178)))), ((int)(((byte)(191)))));
+            this.Form1_but.Location = new System.Drawing.Point(0, 92);
+            this.Form1_but.Name = "Form1_but";
+            this.Form1_but.Size = new System.Drawing.Size(200, 41);
+            this.Form1_but.TabIndex = 1;
+            this.Form1_but.Text = "Form1";
+            this.Form1_but.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.Form1_but.UseVisualStyleBackColor = true;
+            this.Form1_but.Click += new System.EventHandler(this.Form1_but_Click);
+            // 
+            // Form2_but
+            // 
+            this.Form2_but.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Form2_but.FlatAppearance.BorderSize = 0;
+            this.Form2_but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Form2_but.Font = new System.Drawing.Font("Paperlogy 4 Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Form2_but.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(178)))), ((int)(((byte)(191)))));
+            this.Form2_but.Location = new System.Drawing.Point(0, 133);
+            this.Form2_but.Name = "Form2_but";
+            this.Form2_but.Size = new System.Drawing.Size(200, 41);
+            this.Form2_but.TabIndex = 2;
+            this.Form2_but.Text = "Form2";
+            this.Form2_but.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.Form2_but.UseVisualStyleBackColor = true;
+            this.Form2_but.Click += new System.EventHandler(this.Form2_but_Click);
+            // 
+            // Form3_but
+            // 
+            this.Form3_but.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Form3_but.FlatAppearance.BorderSize = 0;
+            this.Form3_but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Form3_but.Font = new System.Drawing.Font("Paperlogy 4 Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Form3_but.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(178)))), ((int)(((byte)(191)))));
+            this.Form3_but.Location = new System.Drawing.Point(0, 174);
+            this.Form3_but.Name = "Form3_but";
+            this.Form3_but.Size = new System.Drawing.Size(200, 41);
+            this.Form3_but.TabIndex = 3;
+            this.Form3_but.Text = "Form3";
+            this.Form3_but.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.Form3_but.UseVisualStyleBackColor = true;
+            this.Form3_but.Click += new System.EventHandler(this.Form3_but_Click);
+            // 
+            // Setting_but
+            // 
+            this.Setting_but.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Setting_but.FlatAppearance.BorderSize = 0;
+            this.Setting_but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Setting_but.Font = new System.Drawing.Font("Paperlogy 4 Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Setting_but.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(178)))), ((int)(((byte)(191)))));
+            this.Setting_but.Location = new System.Drawing.Point(0, 449);
+            this.Setting_but.Name = "Setting_but";
+            this.Setting_but.Size = new System.Drawing.Size(200, 41);
+            this.Setting_but.TabIndex = 4;
+            this.Setting_but.Text = "Settings";
+            this.Setting_but.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.Setting_but.UseVisualStyleBackColor = true;
+            this.Setting_but.Click += new System.EventHandler(this.Setting_but_Click);
+            // 
+            // FormpaL
+            // 
+            this.FormpaL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FormpaL.Location = new System.Drawing.Point(200, 0);
+            this.FormpaL.Name = "FormpaL";
+            this.FormpaL.Size = new System.Drawing.Size(670, 490);
+            this.FormpaL.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(109)))), ((int)(((byte)(130)))));
+            this.ClientSize = new System.Drawing.Size(870, 490);
+            this.Controls.Add(this.FormpaL);
+            this.Controls.Add(this.side_panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(870, 490);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.side_panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel side_panel;
+        private System.Windows.Forms.Panel profile_panel;
+        private System.Windows.Forms.Button Form1_but;
+        private System.Windows.Forms.Button Form3_but;
+        private System.Windows.Forms.Button Form2_but;
+        private System.Windows.Forms.Button Setting_but;
+        private System.Windows.Forms.Panel FormpaL;
+    }
+}
+
---
+++ Modern UI/MainForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace Modern_UI
+{
+    public partial class MainForm : Form
+    {
+        #region Form 설정
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+        public MainForm()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 0, 0)); //Form의 Round처리 (Reight부분을 변경하면 Round처리됨)
+        }
+        Color formcolor = Color.FromArgb(82, 109, 130);
+
+        Size mixsize = new Size(654, 451);
+#endregion
+        #region Buttion 색상
+        private void butdisColor()
+        {
+            Color Disable = Color.FromArgb(39, 55, 77); //비활성화 색상
+            Color Fontdisable = Color.FromArgb(157, 178, 191); //비활성화 폰트색상
+            this.Form1_but.BackColor = Disable;
+            this.Form2_but.BackColor = Disable;
+            this.Form3_but.BackColor = Disable;
+            this.Setting_but.BackColor = Disable;
+
+            //기본 폰트 색상
+            this.Form1_but.ForeColor = Fontdisable;
+            this.Form2_but.ForeColor = Fontdisable;
+            this.Form3_but.ForeColor = Fontdisable;
+            this.Setting_but.ForeColor = Fontdisable;
+        }
+        Color Active = Color.FromArgb(82, 109, 130); //버튼 활성화 색상
+        Color FontActive = Color.FromArgb(0,0,0); //버튼 활성화 색상
+        #endregion
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Form1_but_Click( sender, e );
+        }
+
+        private void Form1_but_Click(object sender, EventArgs e)
+        {
+            butdisColor();
+            this.Form1_but.BackColor = Active;
+            this.Form1_but.ForeColor = FontActive;
+
+            this.FormpaL.Controls.Clear();
+            Form1 form = new Form1()
+            {
+                Dock = DockStyle.Fill, //변경금지
+                TopLevel = false, //변경금지
+                TopMost = true, //변경금지
+                MinimumSize = mixsize, //Form 설정부분에서 Size값 변경
+                Size = mixsize, //Form 설정부분에서 Size값 변경
+
+                BackColor = formcolor //BackColor는 Form의 배경을 지정하므로 예외시 주석처리하여 사용가능
+
+            };
+            form.FormBorderStyle = FormBorderStyle.None;
+            this.FormpaL.Controls.Add(form);
+            form.Show();
+        }
+
+        private void Form2_but_Click(object sender, EventArgs e)
+        {
+            butdisColor();
+            this.Form2_but.BackColor = Active;
+            this.Form2_but.ForeColor = FontActive;
+
+            this.FormpaL.Controls.Clear();
+            Form2 form = new Form2()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true,
+                MinimumSize = mixsize,
+                Size = mixsize,
+
+                BackColor = formcolor
+            };
+            form.FormBorderStyle = FormBorderStyle.None;
+            this.FormpaL.Controls.Add(form);
+            form.Show();
+        }
+
+        private void Form3_but_Click(object sender, EventArgs e)
+        {
+            butdisColor();
+            this.Form3_but.BackColor = Active;
+            this.Form3_but.ForeColor = FontActive;
+
+            this.FormpaL.Controls.Clear();
+            Form3 form = new Form3()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true,
+                MinimumSize = mixsize,
+                Size = mixsize,
+
+                BackColor = formcolor
+            };
+            form.FormBorderStyle = FormBorderStyle.None;
+            this.FormpaL.Controls.Add(form);
+            form.Show();
+        }
+
+        private void Setting_but_Click(object sender, EventArgs e)
+        {
+            butdisColor();
+            this.Setting_but.BackColor = Active;
+            this.Setting_but.ForeColor = FontActive;
+
+            this.FormpaL.Controls.Clear();
+            Setting form = new Setting()
+            {
+                Dock = DockStyle.Fill,
+                TopLevel = false,
+                TopMost = true,
+                MinimumSize = mixsize,
+                Size = mixsize,
+
+                BackColor = formcolor
+            };
+            form.FormBorderStyle = FormBorderStyle.None;
+            this.FormpaL.Controls.Add(form);
+            form.Show();
+        }
+    }
+}
